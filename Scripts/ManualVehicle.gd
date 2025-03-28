ac9fbea01601626827ffa86016d4e2b7d8cbbec9
Ícaro Freire <ivfreire@outlook.com>
2025-03-27 21:43:00 -0300

    :sparkles: some movement

--- Scripts/ManualVehicle.gd
+++ Scripts/ManualVehicle.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 var rpm = 1000
 var gear = 0
-var traction = 50000
+var traction = 15000
 var angle = PI / 6
 
 func _ready():
@@ -11,19 +11,22 @@ func _ready():
 func _physics_process(delta):
 	var is_x_button_pressed = Input.is_action_pressed("ui_accept")
 	
-	angle = - Input.get_joy_axis(0, 0) * PI / 2
+	angle = - Input.get_joy_axis(0, 0) * PI / 6
 	
 	$Wheels/FrontLeft.rotation.y = angle
 	$Wheels/FrontRight.rotation.y = angle
 	
 	var clutch_level = Input.get_joy_axis(0, 4)
 	
-	var force = self.global_transform.basis.z * traction * (1 - clutch_level)
-	force -= force * abs(sin(angle))
+	var _traction = traction * (1 - clutch_level)
+	if is_x_button_pressed: _traction = - 1.2 * _traction
+	
+	var force = global_transform.basis.z * _traction * cos(angle) * cos(angle)
+	force += global_transform.basis.x * _traction * sin(angle) * cos(angle)
 	
 	print(angle, force)
 	
-	var torque = self.global_transform.basis.y * traction * cos(angle) * sin(angle)
+	var torque = self.global_transform.basis.y * 1.4 * _traction * cos(angle) * sin(angle)
 	
 	self.apply_central_force(-force)
 	self.apply_torque(torque)
